944f2370476b07e8acddcd10b2ef7894ba64c785
Shawn Wilson <shawn.wilson137@gmail.com>
2019-03-04 15:46:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/*
 function takeANumber(katzDeliLine,name){
 katzDeliLine.push(name);
 return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+*/
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
