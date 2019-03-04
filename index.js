81132d2e902ffa2b6c25586cbac2e5d37e1dc47d
Shawn Wilson <shawn.wilson137@gmail.com>
2019-03-04 15:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 */
 
+function takeANumber(){
+  
+}
+
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
